d158f03f89988c6b36b3ca687a2e4eec7ddd8489
Rob Turner <rmeturner@gmail.com>
2021-04-09 16:31:04 +0200

    Feat: Add DB Model configuration and check admin setup

--- immfly/settings.py
+++ immfly/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +52,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'immfly.urls'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TEMPLATES = [
     {
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Channel(models.Model):
+    title = models.CharField(max_length=120, null=False, blank=False)
+    language = models.CharField(max_length=2, null=False, blank=False)
+    picture = models.ImageField(upload_to='images/')
+    parent_channel = models.ForeignKey('self', null=True, blank=True, related_name='parent', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+class Content(models.Model):
+    name = models.CharField(max_length=120, null=False, blank=False)
+    content = models.ImageField(upload_to='content/')
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(rating__lte=10.0), name='rating_lte_10.0',)
+        ]
+
+class ContentMeta(models.Model):
+    content_id = models.ForeignKey(Content, on_delete=models.CASCADE)
+    meta_key = models.CharField(max_length=30, null=False, blank=False)
+    meta_value = models.CharField(max_length=120, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.meta_key) + ': ' + str(self.meta_value)
